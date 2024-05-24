bfa37cfb55cde458f9a9401db00c7450fe147325
Vincent <magicdroidx@gmail.com>
2024-05-24 05:38:17 +0100

    feat: update plugin metadata

---
+++ src/endstone_discord/__init__.py
@@ -0,0 +1,3 @@
+from endstone_discord.discord_plugin import DiscordPlugin
+
+__all__ = ["DiscordPlugin"]
---
+++ src/endstone_discord/discord_plugin.py
@@ -0,0 +1,5 @@
+from endstone.plugin import Plugin
+
+
+class DiscordPlugin(Plugin):
+    api_version = "0.4"
